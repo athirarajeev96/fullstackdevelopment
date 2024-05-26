7ac6adc2e565d0bb9834bd85a90ddc7c50af722c
athirarajeev96 <165043114+athirarajeev96@users.noreply.github.com>
2024-05-26 20:58:21 +0530

    Update Feedback.jsx

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import testimonials1 from '../assets/img/testimonials-1.jpg';
+import testimonials2 from '../assets/img/testimonials-2.jpg';
+import testimonials3 from '../assets/img/testimonials-3.jpg';
 
 function Feedback() {
   return (
@@ -9,21 +12,21 @@ function Feedback() {
                 <div className="row">
                     <div className="col-lg-4">
                         <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img className="img-fluid rounded-circle mb-3" src="assets/img/testimonials-1.jpg" alt="..." />
+                            <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-1.jpg" alt="..." />
                             <h5>Margaret E.</h5>
                             <p className="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img className="img-fluid rounded-circle mb-3" src="assets/img/testimonials-2.jpg" alt="..." />
+                            <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-2.jpg" alt="..." />
                             <h5>Fred S.</h5>
                             <p className="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img className="img-fluid rounded-circle mb-3" src="assets/img/testimonials-3.jpg" alt="..." />
+                            <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-3.jpg" alt="..." />
                             <h5>Sarah W.</h5>
                             <p className="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
                         </div>
