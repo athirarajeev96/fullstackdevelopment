c71a44924e98669569c1eb3c4df7fd086e8f8833
athirarajeev96 <athirabahuleyan96@gmail.com>
2024-05-27 17:06:55 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Sidebar from './components/SideBar'
+import Dashboard from './components/Dashboard'
+import AddUser from './components/AddUser'
+import ViewUser from './components/ViewUser'
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+function App() {
+  return <>
+  <div id="wrapper">
+    <BrowserRouter>
+    <Sidebar/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/add-user' element={<AddUser/>}/>
+        <Route path='/view-user' element={<ViewUser/>}/>
+        <Route path="*" element={<Navigate to='/'/>}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
+  </>
+}
+
+export default App
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function SideBar() {
+  return <>
+    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+      <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div className="sidebar-brand-icon rotate-n-15">
+          <i className="fas fa-laugh-wink"></i>
+        </div>
+        <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+      </a>
+      <hr className="sidebar-divider my-0" />
+      <li className="nav-item">
+        <Link to='/' className="nav-link">
+          <i className="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </Link>
+      </li>
+      <hr className="sidebar-divider" /> 
+      <div className="sidebar-heading">
+        Interface
+      </div>
+      <li className="nav-item">
+        <Link to='/add-user' className="nav-link collapsed">
+          <i className="fas fa-fw fa-cog"></i>
+          <span>Add User</span>
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link to='/view-user' className="nav-link collapsed">
+          <i className="fas fa-fw fa-wrench"></i>
+          <span>View User</span>
+        </Link>
+      </li>
+      <hr className="sidebar-divider" />
+    </ul>
+  </>
+}
+
+export default SideBar
