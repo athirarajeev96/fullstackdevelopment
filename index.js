05e84e97b300b87b26807641ec02c0ad6306c488
athirarajeev96 <athirabahuleyan96@gmail.com>
2024-10-07 23:15:26 +0530

    added deployed frontend url

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ import Routes from './routes/index.js';
 import { authMiddleware, adminMiddleware } from './middleware/authMiddleware.js';
 import './models/index.js';  // Ensure MongoDB connection is established
 
-const PORT = process.env.PORT || 8000;  // Use the port you've set or default to 8000
+const PORT = process.env.PORT || 8000;
+const FRONTEND_URL = process.env.FRONTEND_URL; // Add your frontend deployed URL to environment variables
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: FRONTEND_URL, // Allow only your frontend's deployed URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true, // Set to true if you need to allow credentials like cookies, authorization headers, etc.
+}));
 app.use(express.json());
 
 // Mounting the main router for API routes
-app.use('/api', Routes); // All API routes will now start with /api
+app.use('/api', Routes);
 
 // Admin route with middleware
 app.use('/admin', authMiddleware, adminMiddleware, (req, res) => {
