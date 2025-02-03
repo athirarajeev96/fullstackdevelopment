b2cc00e6925782215420d4cbd4c81c33f235ded3
athirarajeev96 <athirabahuleyan96@gmail.com>
2025-02-03 16:34:55 +0530

    deployed url

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,32 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import Routes from './routes/index.js'
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import Routes from './routes/index.js';
 
-const PORT = process.env.PORT
-const app = express()
+const PORT = process.env.PORT;
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+// CORS configuration
+const allowedOrigins = [
+  'https://exquisite-hummingbird-845c16.netlify.app', // Add your frontend URL here
+];
 
-app.use(Routes)
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // Allow requests from the specified frontend URL or from localhost (for development)
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
 
-app.listen(PORT,()=>console.log(`Server Running at port ${PORT}`))
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(Routes);
+
+app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
